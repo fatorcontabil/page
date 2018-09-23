ac49f8f684759a95834394c2d6cb2ddabd9c15ee
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2018-09-23 11:12:41 -0300

    Barra Superior

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def index():
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    return render_template('error.html'), 404
 
 # from app.mod_cad.controllers import mod_cad as cad_module
 # from app.mod_home.controllers import mod_home as home_module
