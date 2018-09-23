2f087047ccaf16ea4fa739ca2fa56016dedf5f37
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2018-09-23 17:55:14 -0300

    inclusão da página de erros genérica

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,11 @@ def index():
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('error.html'), 404
+    return render_template('error.html', error=error, number=404), 404
+
+@app.errorhandler(500)
+def not_found(error):
+    return render_template('error.html', error=error, number=500), 500
 
 # from app.mod_cad.controllers import mod_cad as cad_module
 # from app.mod_home.controllers import mod_home as home_module
